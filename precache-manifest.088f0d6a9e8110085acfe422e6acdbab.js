bcc94c37f8e4739ca68ffa30a4107bd87c033756
mukjo96 <mukjo96@gmail.com>
2020-09-16 22:39:27 +0900

    Updates

---
+++ precache-manifest.088f0d6a9e8110085acfe422e6acdbab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37269cbb72af76fe6fc566682a84896d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "60271c4913cd4b5689e3",
+    "url": "/movie_app/static/css/main.77b6eec7.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60271c4913cd4b5689e3",
+    "url": "/movie_app/static/js/main.dde840ec.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);

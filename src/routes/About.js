e008e67d984f276a48f26c71d1bdd7651c8c0b5f
mukjo96 <mukjo96@gmail.com>
2020-09-17 19:20:36 +0900

    movie_app 0.0.1

---
+++ src/routes/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>"About..."</span>
+      <span>Me</span>
+    </div>
+  );
+}
+
+export default About;

7fd6b1c23af6289cde847dbd7864495a2dc49ded
mukjo96 <mukjo96@gmail.com>
2020-09-25 16:40:57 +0900

    Updates

---
+++ precache-manifest.c17168462be9c57fbef2d2d24495b761.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e6365e77ffd761b9b977fd37ebb31f",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "15bea8bee3e5f7a42b98",
+    "url": "/movie_app/static/css/main.95ad3811.chunk.css"
+  },
+  {
+    "revision": "108ac2968baea8b88904",
+    "url": "/movie_app/static/js/2.43f231aa.chunk.js"
+  },
+  {
+    "revision": "15bea8bee3e5f7a42b98",
+    "url": "/movie_app/static/js/main.597a9826.chunk.js"
+  },
+  {
+    "revision": "71f19a8d0686985f96b8",
+    "url": "/movie_app/static/js/runtime-main.b13a569b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.48f2b3d96a0434c780eacdcbc56e3d68.js"
+  "/movie_app/precache-manifest.c17168462be9c57fbef2d2d24495b761.js"
 );
 
 self.addEventListener('message', (event) => {

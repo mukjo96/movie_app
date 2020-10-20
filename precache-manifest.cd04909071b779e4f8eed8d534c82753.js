3cdfbc284a020dabdf9f713cc3284cc9d4d0a2c9
mukjo96 <mukjo96@gmail.com>
2020-10-20 15:43:23 +0900

    Updates

---
+++ precache-manifest.cd04909071b779e4f8eed8d534c82753.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6443d7e6421434d2f82152ae6c8f4b",
+    "url": "/Movie_app/index.html"
+  },
+  {
+    "revision": "d2f83282e2a9d36010f2",
+    "url": "/Movie_app/static/css/2.41afa381.chunk.css"
+  },
+  {
+    "revision": "0a18940e195df08fcd7e",
+    "url": "/Movie_app/static/css/main.06451828.chunk.css"
+  },
+  {
+    "revision": "d2f83282e2a9d36010f2",
+    "url": "/Movie_app/static/js/2.8cb249aa.chunk.js"
+  },
+  {
+    "revision": "0a18940e195df08fcd7e",
+    "url": "/Movie_app/static/js/main.878fe2fa.chunk.js"
+  },
+  {
+    "revision": "53695d9f457c70e116ac",
+    "url": "/Movie_app/static/js/runtime-main.776da7c4.js"
+  },
+  {
+    "revision": "f10c3a18a69cd6cf6816b1b474d56d77",
+    "url": "/Movie_app/static/media/cgv.f10c3a18.png"
+  },
+  {
+    "revision": "ca0697c0ba1fdcd9115895316bfa523a",
+    "url": "/Movie_app/static/media/logo512.ca0697c0.png"
+  },
+  {
+    "revision": "c31f6ef4ad80eb8b6f6268d4da2ee2f8",
+    "url": "/Movie_app/static/media/lottecinema.c31f6ef4.png"
+  },
+  {
+    "revision": "30c228ba2a75caa41a91e772d2c0945c",
+    "url": "/Movie_app/static/media/megabox.30c228ba.png"
+  }
+]);
